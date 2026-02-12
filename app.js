976f17f8e30a390501c36bdb34086033ded17507
looker775 <kaliwill3@gmail.com>
2026-02-13 05:44:00 +0600

    Add PayPal client ID

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://wuxhbmqvawwkkphveaxc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1eGhibXF2YXd3a2twaHZlYXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4OTIxODQsImV4cCI6MjA4NjQ2ODE4NH0.JUbr-P_qcKLXWV_1LXNewfaZUIb_ngrW3gUmuIc-e2g";
-const PAYPAL_CLIENT_ID = "";
+const PAYPAL_CLIENT_ID = "AWl0VYwK-I-dDMHr041A-zEJ3dVCoTgMTgA43fh2xVROlFwhjWbtVxJKrcwA5jq_04aetzVfOCYFQHbP";
 const PAYPAL_CURRENCY = "USD";
 
 const DEFAULT_PRODUCTS = [
